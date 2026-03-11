ad6c5a8762130ea26ee4037f7f56db1e02978b0f
tsu-masaki <m-tsudura@tency.co.jp>
2026-03-11 18:05:09 +0900

    メール認証修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: ENV["apppass0322@gmail.com"],
-    password: ENV["agrwumqljmztyiju"],
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_APP_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true
   }
