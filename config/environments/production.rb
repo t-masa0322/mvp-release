0f86ab8873c628f65c987c7983926685cf8102e3
tsu-masaki <m-tsudura@tency.co.jp>
2026-03-11 19:41:44 +0900

    fix:メール認証 resend利用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,19 +40,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   config.action_mailer.default_url_options = { host: "mvp-release.onrender.com", protocol: "https" }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :resend
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_APP_PASSWORD"],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
