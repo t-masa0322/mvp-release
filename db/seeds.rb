afd4bed2a4e4969e95f01c31a6e6741288495cd1
tsu-masaki <m-tsudura@tency.co.jp>
2026-03-15 03:49:01 +0900

    fix: seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,6 @@ tulip = Plant.find_or_initialize_by(name: "チューリップ")
 tulip.description = "ほどよいペースで成長を楽しめる植物です。"
 tulip.save!
 
-sunflower_image_path = Rails.root.join("db/seeds/images/sunflower.jpeg")
-if File.exist?(sunflower_image_path) && !sunflower.image.attached?
-  sunflower.image.attach(
-    io: File.open(sunflower_image_path),
-    filename: "sunflower.jpeg"
-  )
-end
-
-cactus_image_path = Rails.root.join("db/seeds/images/cactus.jpeg")
-if File.exist?(cactus_image_path) && !cactus.image.attached?
-  cactus.image.attach(
-    io: File.open(cactus_image_path),
-    filename: "cactus.jpeg"
-  )
-end
-
-tulip_image_path = Rails.root.join("db/seeds/images/tulip.jpeg")
-if File.exist?(tulip_image_path) && !tulip.image.attached?
-  tulip.image.attach(
-    io: File.open(tulip_image_path),
-    filename: "tulip.jpeg"
-  )
-end
-
 [
   [sunflower, [["たね", 1, 0], ["発芽", 2, 2500], ["開花", 3, 8000]]],
   [cactus, [["たね", 1, 0], ["発芽", 2, 3000], ["開花", 3, 9000]]],
